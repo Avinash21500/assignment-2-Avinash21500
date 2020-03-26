540c427d82eb3b3d99a758de4546154bc3f34493
Avinash Dubey <avinash.dubey_da18@gla.ac.in>
2020-03-26 23:26:04 +0530

    made Priority Queue using Linked List

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,89 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    private Node front;
+    private Node rear;
+    private int size;
+
+    //Constructor
+    MyPriorityQueue(){
+        front=null;
+        rear=null;
+        size=0;
+    }
+
+
+    public boolean isEmpty(){
+        boolean response=false;
+        if(front!=null || size!=0){
+            response=true;
+        }
+        return response;
+    }
+    //removing single element from starting of queue
+    public Node dequeue(){
+        Node response=null;
+        if(isEmpty()){
+            if(front.getNext()!=null){
+                response=new Node(front.getData());
+                front=front.getNext();
+                rear.setNext(front);
+                size--;
+            }
+            else {
+                response=new Node(front.getData());
+                front=null;
+                rear=null;
+                size--;
+            }
+        }
+        return response;
+    }
+    //inserting data
+    public void  enqueue(int data){
+        Node node=new Node(data);
+        if(front==null){
+            front=node;
+            size++;
+            System.out.println("Value Entered successfully!");
+        }
+        else if(data<=front.getData()){
+            node.setNext(front);
+            front=node;
+            size++;
+            System.out.println("Value Entered successfully!");
+        }
+        else if(data>=rear.getData()){
+            node.setNext(front);
+            rear.setNext(node);
+            rear=node;
+            System.out.println("Value Entered successfully!");
+        }
+        else {
+            Node previous=null;
+            Node temp = front;
+            while(temp.getNext().getNext()!=null){
+                previous=temp;
+                if(data<=temp.getData()){
+                    previous.setNext(node);
+                    node.setNext(temp);
+                    size++;
+                    System.out.println("Value Entered successfully!");
+                }
+                temp=temp.getNext();
+            }
+        }
+    }
+
+    //traversing
+    public void traversing(){
+        Node temp=front;
+        while(temp.getNext()!=null){
+            System.out.print(temp.getData()+" ");
+            temp=temp.getNext();
+        }
+    }
 }
